6c695db4f2244ac16d8e39904def1442dc4a17e7
karkato <vallauris13@gmail.com>
2018-10-15 17:43:23 +0200

    DAO implementation v2

--- src/DAO/CompagniesDAO.java
+++ src/DAO/CompagniesDAO.java
@@ -4,13 +4,34 @@ package DAO;
 import java.util.List;
 
 import model.Companies;
+import model.Computers;
 
-public interface CompagniesDAO {
+public class CompagniesDAO extends DAO<Computers>{
 
 
 	
 		    List<Companies> findAll();
 		    List<Companies> findById();
 		    List<Companies> findByName();
+			@Override
+			public Computers find(long id) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			@Override
+			public Computers create(Computers obj) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			@Override
+			public Computers update(Computers obj) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+			@Override
+			public void delete(Computers obj) {
+				// TODO Auto-generated method stub
+				
+			}
 
 }
--- src/DAO/ComputerDAO.java
+++ src/DAO/ComputerDAO.java
@@ -3,14 +3,40 @@ import java.util.List;
 
 import model.Computers;
 
-public interface ComputerDAO {
+public class ComputerDAO extends DAO<Computers> {
 	
 
 	    List<Computers> findAll();
 	    List<Computers> findById();
 	    List<Computers> findByName();
-	    boolean insertComputer(Computers computer);
-	    boolean updateComputer(Computers computer);
-	    boolean deleteComputer(Computers computer);
+	    boolean insertComputer(Computers computer) {
+			return false;
+		}
+	    boolean updateComputer(Computers computer) {
+			return false;
+		}
+	    boolean deleteComputer(Computers computer) {
+			return false;
+		}
+		@Override
+		public Computers find(long id) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		@Override
+		public Computers create(Computers obj) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		@Override
+		public Computers update(Computers obj) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		@Override
+		public void delete(Computers obj) {
+			// TODO Auto-generated method stub
+			
+		}
 
 }
