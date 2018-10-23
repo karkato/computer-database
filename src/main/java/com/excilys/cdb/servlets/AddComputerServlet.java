145cedbc3fff923b55e80ee5a12ffb0a53db5696
karkato <vallauris13@gmail.com>
2018-10-23 16:51:42 +0200

    [Dev] Addind editcomputer and addcomputer servlet + modif on the jsp files

---
+++ src/main/java/com/excilys/cdb/servlets/AddComputerServlet.java
@@ -0,0 +1,24 @@
+package com.excilys.cdb.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddComputerServlet extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response )	throws ServletException, IOException {
+		String message = "Transmission de variables : ok ! ";
+		request.setAttribute("test", message);
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/views/addComputer.jsp" ).forward( request, response );
+
+	}
+
+}
