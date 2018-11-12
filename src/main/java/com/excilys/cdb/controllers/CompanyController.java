ea03e43962ccd9e4b3df69baab177d5e380b8873
karkato <vallauris13@gmail.com>
2018-11-12 17:36:17 +0100

    [Dev] Computer and Company controllers implementeds

--- src/main/java/com/excilys/cdb/controllers/CompanyController.java
+++ src/main/java/com/excilys/cdb/controllers/CompanyController.java
@@ -1,25 +1,39 @@
 package com.excilys.cdb.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.ArrayList;
+import java.util.List;
 
-@Controller
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.excilys.cdb.model.Company;
+import com.excilys.cdb.service.CompanyService;
+
+@Controller("companyController")
 @RequestMapping("/Company")
 public class CompanyController {
 	
 	
-	@RequestMapping(method = RequestMethod.GET)
-    public String sayHello(ModelMap model) {
-        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-        return "welcome";
-    }
- 
-    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-    public String sayHelloAgain(ModelMap model) {
-        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-        return "welcome";
-    }
+private final CompanyService companyService;
+	
+	public CompanyController(CompanyService companyService) {
+		this.companyService = companyService;
+	}
+
+	@GetMapping(value = "")
+	public ResponseEntity<List<Company>> findAll() {
+		List<Company> companyList = new ArrayList<Company>();
+		companyList = companyService.findAll();
+		return new ResponseEntity<List<Company>>(companyList, HttpStatus.OK);
+	}
+
+	@DeleteMapping(value = "")
+	public ResponseEntity<Void> delete(long id) {
+		companyService.delete(id);
+		return new ResponseEntity<Void>(HttpStatus.GONE);
+	}
 
 }
--- src/main/java/com/excilys/cdb/controllers/ComputerController.java
+++ src/main/java/com/excilys/cdb/controllers/ComputerController.java
@@ -1,25 +1,94 @@
 package com.excilys.cdb.controllers;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-@Controller
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.excilys.cdb.exceptions.DataException;
+import com.excilys.cdb.exceptions.NoNextPageException;
+import com.excilys.cdb.exceptions.NoPreviousPageException;
+import com.excilys.cdb.model.Computer;
+import com.excilys.cdb.model.Page;
+import com.excilys.cdb.service.ComputerService;
+
+@Controller("computerController")
 @RequestMapping("/Computer")
 public class ComputerController {
 
+	private final ComputerService computerService;
+
+	public ComputerController(ComputerService computerService) {
+		this.computerService = computerService;
+	}
+
+
 	@GetMapping
-	public String sayHello(ModelMap model) {
-		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-		return "welcome";
+	public ResponseEntity<Optional <Computer> > find(long id){
+		Optional<Computer> computer = computerService.find(id);
+		if (computer == null) {
+			return new ResponseEntity<Optional <Computer>>(HttpStatus.NOT_FOUND);
+		}
+
+		return new ResponseEntity<Optional <Computer>>(computer, HttpStatus.OK);
 	}
 
-	@RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap model) {
-		model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-		return "welcome";
+	@GetMapping(value = "/count")
+	public ResponseEntity <Integer> count(String name){
+		int count = computerService.count(name);
+		return new ResponseEntity <Integer> (count,HttpStatus.OK);
 	}
 
+	@GetMapping(value = "/all")
+	public ResponseEntity<List<Computer>> findAll(String name) {
+		List<Computer> computerList = new ArrayList<Computer>();
+		try {
+			computerList = computerService.findAll(name);
+		} catch (NoPreviousPageException e) {
+			Page.increasePage();
+		} catch (NoNextPageException e) {
+			Page.decreasePage();
+		}
+		if (computerList.isEmpty()) {
+			return new ResponseEntity<List<Computer>>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<List<Computer>>(computerList, HttpStatus.OK);
+
+	}
+	
+	@PostMapping
+	public ResponseEntity<Computer> create(@RequestBody Computer computer) {
+		try {
+			computerService.create(computer);
+		} catch (DataException e) {
+			e.printStackTrace();
+		}
+		ResponseEntity<Computer> responseEntity = new ResponseEntity<Computer>(computer, HttpStatus.CREATED);
+		return responseEntity;
+		
+	}
+	@PutMapping
+	public ResponseEntity<Computer> update(@RequestBody Computer computer) {
+		try {
+			computerService.update(computer);
+		} catch (DataException e) {
+			e.printStackTrace();
+		}
+		ResponseEntity<Computer> responseEntity = new ResponseEntity<Computer>(computer, HttpStatus.OK);
+		return responseEntity;
+		
+	}
+	@DeleteMapping
+	public ResponseEntity<Void> delete(String[] idTab) {
+		computerService.deleteAll(idTab);
+		return new ResponseEntity<Void>(HttpStatus.GONE);
+	}
 }
